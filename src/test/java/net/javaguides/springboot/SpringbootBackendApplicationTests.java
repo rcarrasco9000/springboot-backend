ce25c412d958ddac221fa6e60fdbbd850173f236
rcarrasco9000 <rcarrasco9000@users.noreply.github.com>
2020-10-08 23:29:46 -0400

    logging added

--- src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
+++ src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
@@ -17,7 +17,7 @@ class SpringbootBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
-		logger.info("Test case executing....");
+		logger.info("Test case executing......");
 		assertEquals(true,true);
 	}
 
